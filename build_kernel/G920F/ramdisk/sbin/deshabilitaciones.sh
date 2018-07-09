9717c7b991c439767d00bc5848f13b6b5a1407b1
AntonioMartitos <antonio.martos.mendez@gmail.com>
2018-07-09 20:00:53 +0200

    Menor Update Ramdisk

--- build_kernel/G920F/ramdisk/sbin/deshabilitaciones.sh
+++ build_kernel/G920F/ramdisk/sbin/deshabilitaciones.sh
@@ -11,12 +11,6 @@ su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:3/cache_type'
 /sbin/resetprop -n ro.boot.warranty_bit "0"
 /sbin/resetprop -n ro.warranty_bit "0"
 
-# Fix safetynet flags
-/sbin/resetprop -n ro.boot.veritymode "enforcing"
-/sbin/resetprop -n ro.boot.verifiedbootstate "green"
-/sbin/resetprop -n ro.boot.flash.locked "1"
-/sbin/resetprop -n ro.boot.ddrinfo "00000001"
-
 # Samsung related flags
 /sbin/resetprop -n ro.fmp_config "1"
 /sbin/resetprop -n ro.boot.fmp_config "1"
